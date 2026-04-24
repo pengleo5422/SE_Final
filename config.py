74fea81be1279fdeecca16340104b9047ca01172
Bo Sheng Peng <91608305+pengleo5422@users.noreply.github.com>
2026-04-25 04:38:31 +0800

    Update database URI in config.py
    Change SQLALCHEMY_DATABASE_URI to a placeholder value.

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     # 資料庫 URI (修改為您使用的資料庫)
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:OReEeDaejJYLSawJZIaOQTJPbYsOyXhO@junction.proxy.rlwy.net:29883/railway"
+    SQLALCHEMY_DATABASE_URI = "y"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')  # 建議用環境變數設置
